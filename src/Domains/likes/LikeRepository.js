61e6a05ebd49e2b24a7ab1e822332251cd4d19dc
meissy <meissy@rectmedia.com>
2025-05-22 11:50:47 +0700

    pass domain like test case

--- src/Domains/likes/LikeRepository.js
+++ src/Domains/likes/LikeRepository.js
@@ -1,4 +1,15 @@
 class LikeRepository {
+  async addLike(newLike) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyLike(likeData) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteLike(likeData) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = LikeRepository;
--- src/Domains/likes/entities/AddLike.js
+++ src/Domains/likes/entities/AddLike.js
@@ -1,4 +1,16 @@
 class AddLike {
+  constructor({ commentId, userId }) {
+    if (!commentId || !userId) {
+      throw new Error('NEW_LIKE.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof commentId !== 'string' || typeof userId !== 'string') {
+      throw new Error('NEW_LIKE.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+
+    this.commentId = commentId;
+    this.userId = userId;
+  }
 }
 
 module.exports = AddLike;
